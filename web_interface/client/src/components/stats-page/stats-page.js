5a8da26f815614258558a758075ddc8269cd16a1
Mauran Pavan <mauranpavan@gmail.com>
2022-08-04 17:49:45 -0400

    deletedd stats.hooks

--- web_interface/client/src/components/stats-page/stats-page.js
+++ web_interface/client/src/components/stats-page/stats-page.js
@@ -12,21 +12,21 @@ export const Stats = () => {
             </nav>
             <p>Here are the overall statistics</p>
             <ul>
-               <li>Retrieve all WorkoutSummary records gathered by Group 8 
-               <p><a href="./web_interface/data/workout-summaries-stats.html">Workout Summaries</a></p>
-               </li>
+                <li>Retrieve all WorkoutSummary records gathered by Group 8
+                    <p><a href="./web_interface/data/workout-summaries-stats.html">Workout Summaries</a></p>
+                </li>
                 <li>Aggregated WorkoutSummary data in terms of Daily Occurance
-                <p><a href="./web_interface/data/daily-stats.html">Daily</a></p>
+                    <p><a href="./web_interface/data/daily-stats.html">Daily</a></p>
 
                 </li>
                 <li>Aggregated WorkoutSummary data in terms of Weekly Occurance (using the previous occurance)
-                <p><a href="./web_interface/data/weekly-stats.html">Weekly</a></p>
+                    <p><a href="./web_interface/data/weekly-stats.html">Weekly</a></p>
                 </li>
                 <li>Aggregated WorkoutSummary data in terms of Monthly Occurance (using the previous occurance)
-                <p><a href="./web_interface/data/monthly-stats.html">Monthly</a></p>
+                    <p><a href="./web_interface/data/monthly-stats.html">Monthly</a></p>
                 </li>
                 <li>Aggregated WorkoutSummary data in terms of Yearly Occurance (using the previous occurance)
-                <p><a href="./web_interface/data/yearly-stats.html">Yearly</a></p>
+                    <p><a href="./web_interface/data/yearly-stats.html">Yearly</a></p>
                 </li>
             </ul>
         </div>
